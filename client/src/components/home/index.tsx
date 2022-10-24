a9ca0c1a3a2378f9d3cd605d92f5098f1c6032f9
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-10-24 15:58:33 +0200

    added home screen for logging in and styles + animations

---
+++ client/src/components/home/index.tsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+const Home = () => {
+  return (
+    <div className="background">
+      <div className="sun"></div>
+      <div className="sky"></div>
+      <div className="clouds"></div>
+      <div className="title">
+        <h4>WIKTOR RUDZKI</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
